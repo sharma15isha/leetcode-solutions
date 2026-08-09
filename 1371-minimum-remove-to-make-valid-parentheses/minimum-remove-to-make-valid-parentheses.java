d8afa83645074e6b0bb5311e3daea6821a62d35c
Isha Sharma <ishasharma3061@gmail.com>
2026-08-09 18:21:53 +0530

    Time: 19 ms (64.08%) | Memory: 47 MB (93.43%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,34 +1,32 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-       StringBuilder result=new StringBuilder();
-       int count=0;
+      StringBuilder result=new StringBuilder();
+     int count=0;
 
-       for(int i=0;i<s.length();i++){
+      for(int i=0;i<s.length();i++){
         char ch=s.charAt(i);
-
         if(ch == '('){
             count++;
+
             if(count>0){
                 result.append(ch);
             }
-
         }
+
         else if(ch == ')'){
             if(count>0){
                 count--;
                 result.append(ch);
             }
-
         }
+
         else{
-            result.append(ch);
-
+           result.append(ch);
         }
-       }
-
-       StringBuilder answer=new StringBuilder();
-       for(int i=result.length()-1;i>=0;i--){
-        char ch=result.charAt(i);
+      }
+      StringBuilder answer=new StringBuilder();
+      for(int i=result.length()-1;i>=0;i--){
+        char ch= result.charAt(i);
 
         if(ch == '(' && count>0){
             count--;
@@ -36,7 +34,9 @@ class Solution {
         else{
             answer.append(ch);
         }
-       }
-       return answer.reverse().toString();
+      }
+      return answer.reverse().toString();
+
+
     }
 }
