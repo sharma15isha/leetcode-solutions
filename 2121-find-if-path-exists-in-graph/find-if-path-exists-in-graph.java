30ead1866c2339c7e5c279dedec25125b949da2e
Isha Sharma <ishasharma3061@gmail.com>
2026-09-03 11:38:24 +0530

    Time: 67 ms (76.56%) | Memory: 282 MB (43.79%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,39 +1,39 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-     List<List<Integer>> list= new ArrayList<>();
+   List<List<Integer>> graph=new ArrayList<>();
 
-     for(int i=0;i<n;i++){
-        list.add(new ArrayList<>());
-     }
+   for(int i=0;i<n;i++){
+    graph.add(new ArrayList<>());
+   }
 
-     for(int[] pair: edges){
-        int u=pair[0];
-        int v=pair[1];
+   for(int[] pair : edges ){
+    int u=pair[0];
+    int v=pair[1];
 
-        list.get(u).add(v);
-        list.get(v).add(u);  
-     }
+    graph.get(u).add(v);
+    graph.get(v).add(u);
 
-     boolean[] visited=new boolean[n];
-     Queue<Integer> q= new LinkedList<>();
+   }
 
-     q.add(source);
-     visited[source]=true;
+   boolean[] visited= new boolean[n];
+   Queue<Integer> q=new LinkedList<>();
 
-     while(!q.isEmpty()){
-        int curr=q.poll();
+   q.add(source);
+   visited[source]=true;
 
-        if(curr == destination){
-            return true;
+   while(!q.isEmpty()){
+    int curr=q.poll();
+    if(curr == destination){
+        return true;
+    }
+
+    for(int neighbour : graph.get(curr)){
+        if(!visited[neighbour]){
+            visited[neighbour]=true;
+            q.add(neighbour);
         }
-
-        for(int neighbour : list.get(curr)){
-            if( !visited[neighbour]){
-                visited[neighbour]=true;
-                q.add(neighbour);
-            }
-        }
-     }
-     return false;
+    }
+   }
+    return false;
     }
 }
